a4137a89ee551e11cda319829496f2c6a6d7cbf6
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-07-27 17:50:34 +0900

    [Silver I] Title: IOIOI, Time: 620 ms, Memory: 186712 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,14 +1,34 @@
-N = int(input())
-p_n = 'O'.join('I' * (N + 1))
-length = 2*N + 1
+from collections import deque
 
+N = int(input())
 M = int(input())
 S = input()
 
+queue = deque([])
+length = 0
 result = 0
 
-for idx in range(M - 2 * N):
-    if S[idx:idx + length] == p_n:
-        result += 1
+for ch in S:
+    if not queue:
+        if ch == 'I':
+            queue.append(ch)
+            length = 1
+        continue
+    if queue[0] == 'O':
+        queue.popleft()
+        length -= 1
+    if queue[-1] == ch:
+        queue = deque([])
+        length = 0
+        if ch == 'I':
+            queue.append(ch)
+            length = 1
+    else:
+        queue.append(ch)
+        length += 1
+        if length == (2 * N + 1) and queue[0] == 'I':
+            result += 1
+            length -= 1
+            queue.popleft()
 
 print(result)
