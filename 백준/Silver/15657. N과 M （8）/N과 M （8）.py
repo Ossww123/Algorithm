0b6af16ed712e6b788cdaced51a8b2752e4965fa
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-08-02 02:07:47 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 113784 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort() # 1 7 8 9
+
+def backtrack(result, idx):
+    if len(result) == M:
+        print(*result)
+        return
+
+    for i in range(idx, N):
+        result.append(lst[i])
+        backtrack(result, i)
+        result.pop()
+
+
+backtrack([], 0)
