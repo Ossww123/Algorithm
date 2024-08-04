3579496ec81659d216ae0d9a1bcc7499f4493862
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-08-04 22:16:05 +0900

    [D2] Title: 간단한 소인수분해, Time: 97 ms, Memory: 43,256 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    result = [0] * 5
+    while N % 2 == 0:
+        N = N // 2
+        result[0] += 1
+    while N % 3 == 0:
+        N = N // 3
+        result[1] += 1
+    while N % 5 == 0:
+        N = N // 5
+        result[2] += 1
+    while N % 7 == 0:
+        N = N // 7
+        result[3] += 1
+    while N % 11 == 0:
+        N = N // 11
+        result[4] += 1
+
+    print(f"#{tc} {' '.join(map(str, result))}")
