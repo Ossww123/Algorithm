16337a23587e7a727e8703452b5a6fba18aa9e29
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-08-21 09:30:49 +0900

    [Platinum V] Title: 행성 터널, Time: 1044 ms, Memory: 172880 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,44 +1,28 @@
 N = int(input())
 
-# 행성들의 x, y, z 좌표를 따로 저장
-x_planets = []
-y_planets = []
-z_planets = []
+# 행성들의 좌표를 저장
+planets = []
 
 for i in range(N):
     x, y, z = map(int, input().split())
-
-    x_planets.append((x, i))
-    y_planets.append((y, i))
-    z_planets.append((z, i))
-
-# 인접한 행성들을 알기 위해 정렬
-x_planets.sort()
-y_planets.sort()
-z_planets.sort()
-
+    planets.append((x, y, z, i))
+    
 # 인접한 좌표끼리 간선 만들기
 edges = []
-for i in range(N - 1):
-    x1, ix1 = x_planets[i]
-    x2, ix2 = x_planets[i + 1]
+for j in range(3):
+    planets.sort(key=lambda k: k[j])
+    for i in range(N - 1):
+        u, i1 = planets[i][j], planets[i][3]
+        v, i2 = planets[i + 1][j], planets[i + 1][3]
 
-    y1, iy1 = y_planets[i]
-    y2, iy2 = y_planets[i + 1]
-
-    z1, iz1 = z_planets[i]
-    z2, iz2 = z_planets[i + 1]
-
-    edges.append((ix1, ix2, abs(x2 - x1)))
-    edges.append((iy1, iy2, abs(y2 - y1)))
-    edges.append((iz1, iz2, abs(z2 - z1)))
-
+        edges.append((i1, i2, abs(u - v)))
 
 # 크루스칼 알고리즘
 edges.sort(key=lambda k: k[2])
 
 # 유니온 파인드
 parent = list(range(N))
+
 
 def find(x):
     if parent[x] != x:
