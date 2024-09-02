e41e4f1c254f75101ce28441bef61f1ac4759738
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-02 22:28:59 +0900

    [D4] Title: 준환이의 양팔저울, Time: 705 ms, Memory: 74,492 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
@@ -9,13 +9,24 @@ def remained(M):
 
 
 # 순열
-def permutation(remain, idx=0,w_diff=0):
+def permutation(remain, idx=0, w_diff=0):
     global result
+    global memo
+
+    # 메모이제이션 체크
+    state = (tuple(visited), remain, idx, w_diff)
+    if state in memo:
+        result += memo[state]
+        return
 
     # 왼쪽과 오른쪽의 무게차보다 남아있는 추의 무게합이 적으면
     if remain <= w_diff:
-        result += remained(N - idx)
+        remaining_count = remained(N - idx)
+        result += remaining_count
+        memo[state] = remaining_count
         return
+
+    current_result = result
 
     # 순열, 왼쪽에 올리면 + weights[i], 오른쪽에 올리면 - weights[i]
     for i in range(N):
@@ -28,14 +39,17 @@ def permutation(remain, idx=0,w_diff=0):
                 permutation(remain - weights[i], idx + 1, w_diff - weights[i])
                 visited[i] = False
 
+    # 결과 계산 후 메모이제이션에 저장
+    memo[state] = result - current_result
 
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     weights = list(map(int, input().split()))
     visited = [False] * N
     all_weight = sum(weights)
+    memo = {}
 
     result = 0
 
