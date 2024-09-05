403a7b869e9f68602d899afe96146f609f6a725c
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-05 11:04:10 +0900

    [Bronze V] Title: 사파리월드, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		long num1 = sc.nextInt();
+		long num2 = sc.nextInt();
+		
+		System.out.println(Math.abs(num1-num2));
+		
+		
+	}
+}
