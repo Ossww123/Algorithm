0bb856174df3267aca169fff4ee2136d52caa006
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-05 11:08:45 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n, m;
+	cin >> n >> m;
+	if (n - m > 0)
+		cout << n - m << '\n';
+	else
+		cout << -(n - m) << '\n';
+	return 0;
+}
