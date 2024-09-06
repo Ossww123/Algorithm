fc3883248e629be12c96a0857866bccaf9a8a8a4
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-06 12:04:11 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+
+	int f = a + b + c + d + e;
+	cout << f;
+
+
+	return 0;
+}
