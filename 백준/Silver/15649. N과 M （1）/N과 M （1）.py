e103c170c6ec08039db338c375af0f9bca7ea60b
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-09 09:10:22 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 115184 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,14 @@
-N, M = map(int, input().split()) # 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-arr = [0 for _ in range(M)]
-used = [0 for _ in range(N+1)]
-
-def backtracking(idx):
-    if idx == M:
-        print(' '.join(map(str, arr)))
+def solve(lst = []):
+    if len(lst) == M:
+        print(*lst)
         return
 
     for i in range(1, N+1):
-        if used[i] == 0:
-            arr[idx] = i
-            used[i] = 1
-            backtracking(idx + 1)
-            used[i] = 0
+        if i not in lst:
+            lst.append(i)
+            solve(lst)
+            lst.pop()
 
-backtracking(0)
+
+N, M = map(int,input().split())
+solve()
