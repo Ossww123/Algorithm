35dbcd8a98e3de1f7b1cc189d85686f401f53f9a
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-09 09:13:01 +0900

    [Silver III] Title: N과 M (2), Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,16 +1,13 @@
-N, M = map(int, input().split())
-
-def backtrack(result, idx):
-    if len(result) == M:
-        print(*result)
+def solve(lst = [], idx = 1):
+    if len(lst) == M:
+        print(*lst)
         return
 
     for i in range(idx, N+1):
-        if not i in result:
-            result.append(i)
-            idx += 1
-            backtrack(result, i)
-            idx -= 1
-            result.pop()
+        if i not in lst:
+            lst.append(i)
+            solve(lst, i)
+            lst.pop()
 
-backtrack([], 1)
+N, M = map(int, input().split())
+solve()
