960b0ec27f1a5eaffd9862d3877f1b6a17c2a716
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-09-11 11:02:28 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) {
+		String art = "\\    /\\\n" +
+					 " )  ( ')\n" +
+					 "(  /  )\n" +
+					 " \\(__)|";
+		System.out.println(art);
+	}
+}
