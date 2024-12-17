099118a7d209bd4e624362afb4457060728e7a3f
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-12-18 06:44:52 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int A, B;
+
+        cout << "yes" << endl;
+    }
+
+    return 0;
+}
