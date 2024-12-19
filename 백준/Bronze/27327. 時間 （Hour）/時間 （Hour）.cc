a8e0886259ca1a6d6b468469be33ad8a54a90d12
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2024-12-20 06:00:24 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    cout << 24 * N;
+    
+
+    return 0;
+}
