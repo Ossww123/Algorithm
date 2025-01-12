6d24b82418c2ed33b28accbb364246ded024821f
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-01-12 19:22:47 +0900

    [Gold V] Title: 수 고르기, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -15,31 +16,21 @@ int main() {
         nums.push_back(num);
     }
 
-    int L = 0, R = 0;
     sort(nums.begin(), nums.end());
 
-    int diff, min_diff = -1;
-    while (R < N) {
-        diff = nums[R] - nums[L];
+    int L = 0, R = 0;
+    int min_diff = INT_MAX;
 
-        if (diff > M) {
-            if (min_diff == -1) {
-                min_diff = diff;
-            }
-            else {
-                min_diff = min(min_diff, diff);
-                L++;
-            }
-        }
-        else if (diff == M) {
-            min_diff = diff;
-            break;
+    while (R < N) {
+        int diff = nums[R] - nums[L];
+
+        if (diff >= M && L < R) {
+            min_diff = min(min_diff, diff);
+            L++;
         }
         else {
-            if (R < N) R++;
-            else break;
+            R++;
         }
-        
     }
 
     cout << min_diff;
