a893bfcdf48e4009b4b0cebd1991b9897a9dcd94
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-02-03 21:50:55 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << N - 2024;
+
+    return 0;
+}
