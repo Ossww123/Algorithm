64c3981b7b7e3476d8b1919808470751efe0465b
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-02-03 21:47:38 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -3,34 +3,36 @@
 
 using namespace std;
 
-void backtracking(int N, int M, vector<int>& seq, vector<bool>& used) {
-	if (seq.size() == M) {
-		for (int i = 0; i < M; ++i) {
-			cout << seq[i] << " ";
-		}
-		cout << "\n";
-		return;
-	}
+vector<int> nums;
+vector<bool> visited(9, false);
 
-	for (int i = 1; i <= N; i++) {
-		if (!used[i]) {
-			used[i] = true;
-			seq.push_back(i);
-			backtracking(N, M, seq, used);
-			seq.pop_back();
-			used[i] = false;
-		}
-	}
+void backtracking(int N, int M,int idx = 0) {
+    if (idx == M) {
+        for (int i = 0; i < M; i++) {
+            cout << nums[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (visited[i]) continue;
+        nums.push_back(i);
+        visited[i] = true;
+        backtracking(N, M, idx + 1);
+        visited[i] = false;
+        nums.pop_back();
+    }
 }
 
 int main() {
-	int N, M;
-	cin >> N >> M;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	vector<int> seq;
-	vector<bool> used(N + 1, false);
+    int N, M;
+    cin >> N >> M;
 
-	backtracking(N, M, seq, used);
+    backtracking(N, M);
 
-	return 0;
+    return 0;
 }
