34e4438153528f15d6a90b4871ada9dbee347ce2
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-02-07 22:04:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
+        }
+
+        for (int j = 0; j < 2 * (N-i)-1; j++) {
+            cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    for (int i = N-2; i >= 0 ; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
+        }
+
+        for (int j = 0; j < 2 * (N-i) - 1; j++) {
+            cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
