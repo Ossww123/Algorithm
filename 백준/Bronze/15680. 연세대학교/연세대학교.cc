b05aeaf7d0f02367d30fa2dd6b0c354d25158fe2
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-03-28 09:14:47 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N){
+        cout << "Leading the Way to the Future";
+    }
+    else{
+        cout << "YONSEI";
+    }
+    
+    return 0;
+}
