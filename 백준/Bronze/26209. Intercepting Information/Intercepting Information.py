7b8d4ef57c34e3a05f3d8f8efa03427432afaeba
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2025-05-11 19:56:04 +0900

    [Bronze V] Title: Intercepting Information, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.py
@@ -0,0 +1,20 @@
+a, b, c, d, e, f, g, h = map(int,input().split())
+ans = "S"
+
+if a == 9:
+    ans = "F"
+if b == 9:
+    ans = "F"
+if c == 9:
+    ans = "F"
+if d == 9:
+    ans = "F"
+if e == 9:
+    ans = "F"
+if f == 9:
+    ans = "F"
+if g == 9:
+    ans = "F"
+if h == 9:
+    ans = "F"
+print(ans)
