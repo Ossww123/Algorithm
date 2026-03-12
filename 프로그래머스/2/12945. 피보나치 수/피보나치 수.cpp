93d8326c7ea86b325aa22ca53813ac50ce448c75
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2026-03-13 00:04:13 +0900

    [level 2] Title: 피보나치 수, Time: 0.56 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> dp(n+1, 0);
+    dp[1] = 1;
+    
+    for (int i=2; i<=n; i++)
+    {
+        int temp = dp[i-1] + dp[i-2];
+        dp[i] = temp % 1234567;
+    }
+    
+    return dp[n];
+}
