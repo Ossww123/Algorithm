351c6b67fc03ea3e77d8bddcf2c23897ff7d59b0
Ossww123 <88433181+Ossww123@users.noreply.github.com>
2026-03-27 04:21:08 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    unordered_map<string, int> m;
+
+    for (auto& c : clothes)
+    {
+        m[c[1]]++;
+    }
+
+    int answer = 1;
+
+    for (auto& it : m)
+    {
+        answer *= (it.second + 1);
+    }
+
+    return answer - 1;
+}
